4fdd0a4d7a10d0d38550807c40f40509d06e7d89
Chung <chungc@cc-iMac.local>
2019-02-13 12:04:49 +0800

    To header only files

--- include/Readme.txt
+++ include/Readme.txt
@@ -1,4 +1,6 @@
-The source code requires Eigen C++ library, which is available at
+The source code requires Eigen C++ library, which is included in the subfolder Eigen and also available at
 http://eigen.tuxfamily.org/
 
-main.cpp sample main function that performs the exact and approximate clustering algorithm on the AWGN model in the last Section of the paper. 
+demo.cpp is an example that performs the exact and approximate clustering algorithm on an AWGN model.
+Compile using
+g++ -g -std=c++11 -I. -o demo.out demo.cpp
---
+++ include/demo.cpp
@@ -0,0 +1,66 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <vector>
+#include <random>
+#include <IC/AIC>
+#include <time.h>
+#include <fstream>
+
+using namespace std;
+using namespace Eigen;
+
+int main() {
+	size_t k = 5;
+	double sigma = 1;
+	size_t n = 50;
+
+	cout << "n = " << n << endl;
+
+	MatrixXd A = MatrixXd::Zero(n, n + k);
+	for (size_t i = 0; i < A.rows(); i++) {
+		A(i, i % k) = 1;
+		A(i, k + i) = sigma;
+	}
+	MatrixXd S = A* A.transpose();
+	//cout << "A=[" << A << "]" << endl;
+	//cout << "S=[" << S << "]" << endl;
+
+	// generate the entropy function
+	GaussianEntropy gsf(S);
+	// generate approximate solution via CL tree
+	cout << "Info-clustering by CL tree approximation:" << endl;
+	vector<size_t> first_node, second_node;
+	vector<double> gamma;
+	for (size_t i = 0; i < n; i++) {
+		for (size_t j = 0; j < i; j++) {
+			first_node.push_back(i);
+			second_node.push_back(j);
+			double I = gsf(vector<size_t> {i}) + gsf(vector<size_t> {j}) - gsf(vector<size_t> {i, j});
+			gamma.push_back(I);
+		}
+	}
+	CL cl(n, first_node, second_node, gamma);
+
+	vector<double> cl_gamma = cl.getCriticalValues();
+	cout << "critical values : " << cl_gamma << endl;
+	for (double gamma : cl.getCriticalValues()) {
+		cout << "partition at threshold " << gamma << ":" << cl.getPartition(gamma) << endl;
+	}
+
+	// generate the exact info-clustering solution via AIC
+	cout << "Agglomerative info-clustering:" << endl;
+	AIC psp(gsf);
+	{
+		size_t i = 0;
+		//cout << psp.getPartition(-1) << endl;
+		while (psp.agglomerate(1E-8, 1E-10)) {
+			//cout << "agglomerates to " << psp.getPartition(-1) << " at critical value " << psp.getCriticalValues().back() << endl;
+		}
+	}
+
+	vector<double> psp_gamma = psp.getCriticalValues();
+	cout << "critical values : " << psp_gamma << endl;
+	for (double gamma : psp.getCriticalValues()) {
+		cout << "partition at threshold " << gamma << ":" << psp.getPartition(gamma) << endl;
+	}
+}
